61cdee5aa6dbee5f9d17a18305b965dc9f456ed4
Phil Betley <jpbetley@gmail.com>
2025-04-11 10:36:59 -0400

    route key name

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class, 'index']);
 
-Route::get('/posts/{post:slug}', function (Post $post) {
+Route::get('/posts/{post}', function (Post $post) {
     return $post;
 });
