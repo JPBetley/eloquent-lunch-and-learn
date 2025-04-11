4abb7c767bb8f2c3579fea3d6fcd6a1b3b1a9f9c
Phil Betley <jpbetley@gmail.com>
2025-04-11 10:40:39 -0400

    scope bindings

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,18 @@
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\UsersController;
 use App\Models\Post;
+use App\Models\PostTag;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class, 'index']);
 
 Route::get('/posts/{post}', function (Post $post) {
     return $post;
+});
+
+Route::get('/posts/{post}/tags/{postTag}', function (Post $post, PostTag $postTag) {
+    return [
+        'post' => $post,
+        'postTag' => $postTag,
+    ];
 });
