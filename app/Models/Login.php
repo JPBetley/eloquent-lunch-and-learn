c545503d8c601129195369716e4db64d4f22ad4e
Phil Betley <jpbetley@gmail.com>
2025-04-11 09:09:31 -0400

    pruning

--- app/Models/Login.php
+++ app/Models/Login.php
@@ -2,18 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Login extends Model
 {
     use HasFactory;
+    //use Prunable;
+    use MassPrunable;
 
     public const UPDATED_AT = null;
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->subYear());
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('model:prune')->daily();
+
+Schedule::command('model:prune', [
+    '--model' => [\App\Models\Login::class],
+    '--except' => [\App\Models\User::class],
+])->daily();
